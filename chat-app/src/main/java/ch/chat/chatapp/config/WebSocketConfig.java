05bdb6897bcae0d24fc2801fb22c9e291b72ff58
jstnceornio <justinceronio@outlook.com>
2024-03-07 11:57:27 +0100

    feat: use special color for each new user

--- chat-app/src/main/java/ch/chat/chatapp/config/WebSocketConfig.java
+++ chat-app/src/main/java/ch/chat/chatapp/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry
                 .addHandler(myHandler(), "/websocket")
-                .setAllowedOrigins("https://demo-chat-app-client.onrender.com");
+                .setAllowedOrigins("http://localhost:4200");
     }
 
     public WebSocketHandler myHandler() {
