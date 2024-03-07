3d03d198f66cf58c4db87c7a2a87bfa3dba983ef
aaronhari <aaron@familiehari.ch>
2024-03-07 08:52:30 +0100

    HOTFIX URL WEBSOCKET

--- chat-app-client/src/app/components/feed/feed.component.ts
+++ chat-app-client/src/app/components/feed/feed.component.ts
@@ -32,9 +32,9 @@ export class FeedComponent implements OnInit, OnDestroy {
   }
   // ws://localhost:8080/websocket
   connect(): void {
-    //this.webSocketService.connect('wss://demo-chat-app-ch5j.onrender.com/websocket', this.username)
+    this.webSocketService.connect('wss://demo-chat-app-ch5j.onrender.com/websocket', this.username)
+    //this.webSocketService.connect('ws://localhost:8080/websocket', this.username);
     this.username = this.username.substring(0, 30)
-    this.webSocketService.connect('ws://localhost:8080/websocket', this.username);
     this.webSocketService.messages.subscribe((message) => {
       this.messages.unshift(message);
     });
